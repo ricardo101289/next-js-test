8b4c2a993ef5bdefbcbf805aa92a0ac19df1a2ee
Daniel Navarrete <dnavarrete@latamautos.com>
2019-10-17 13:18:04 -0500

    Disable next routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,11 @@
-const express = require('express');
-const server = express();
-
-module.exports = app => {
-    const handle = app.getRequestHandler();
-
+module.exports = (app, server) => {
+    server.get('/usados', (req, res) => {
+        const actualPage = '/usados';
+        app.render(req, res, actualPage);
+    });
     server.get('/usados/:brand', (req, res) => {
         const actualPage = '/usados';
         const queryParams = {brand: req.params.brand};
         app.render(req, res, actualPage, queryParams);
     });
-
-    server.get('*', (req, res) => {
-        return handle(req, res)
-    });
-
-    server.listen(3000, (err) => {
-        if (err) throw err;
-        console.log('> Ready on http://localhost:3000');
-    })
 };
