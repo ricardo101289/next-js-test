5f60e9b1b17ae3c56b54ca6067b17395854b9670
Daniel Navarrete <dnavarrete@latamautos.com>
2019-10-18 16:43:50 -0500

    Limit results

--- src/Used/api/UsedVehiclesGraphQLAPI.tsx
+++ src/Used/api/UsedVehiclesGraphQLAPI.tsx
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const GET_VEHICLES = gql`
     {
-        vehiclesConnection {
+        vehiclesConnection(first: 100) {
             edges {
                 node {
                     status
